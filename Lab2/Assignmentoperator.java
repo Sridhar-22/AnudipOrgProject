7458d1fad98f9fd81f6f13778fc60646c9967ad0
Sridhar-22 <112881698+Sridhar-22@users.noreply.github.com>
2024-05-17 13:20:41 +0530

    Lab2

---
+++ Lab2/Assignmentoperator.java
@@ -0,0 +1,14 @@
+package com.Lab2;
+
+public class Assignmentoperator {
+
+	public static void main(String[] args) {
+		
+     int n1 = 56 ;
+     String name = "Sridhar"; 
+     System.out.println("number:"+n1);
+     System.out.println("name:"+name);
+     
+	}
+
+}
---
+++ Lab2/LoginCheckin.java
@@ -0,0 +1,17 @@
+package com.Lab2;
+
+public class LoginCheckin {
+
+	public static void main(String[] args) {
+		String login ="sridhar";
+		if(login == " sai") {
+			System.out.println("user is in valid");
+		}
+		 else{
+		System.out.println("user is logged in");
+		}
+
+	}
+
+}
+  
